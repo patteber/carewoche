05a88c3a66e978e0d21849f93aa0eccef93986c6
Patrick Berg <pberg@gmail.com>
2023-12-04 20:14:33 +0100

    Api POST request for change and iterate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from care import Carewoche
+from pydantic import BaseModel
 
 app = FastAPI()
 #> uvicorn main:app --reload
@@ -9,14 +10,30 @@ async def root():
     return {"message": "Here goes Carewoche"}
 
 @app.get("/members/")
-async def read_item(skip: int = 0, limit: int = 10):
+async def read_members(skip: int = 0, limit: int = 10):
     c = Carewoche('test/resource_t1.json')
     l = list(c.getMembers().items())
     ol = l[skip: skip + limit]
     return dict(ol)
 
 @app.get("/order/")
-async def read_item(skip: int = 0, limit: int = 10):
+async def read_order(skip: int = 0, limit: int = 10):
     c = Carewoche('test/resource_t1.json')
     return c.getOrder()[skip: skip + limit]
     
+class changeOrder(BaseModel):
+    ID: int
+    offset: int
+    
+@app.post("/order/change/")
+async def change_order(co: changeOrder):
+    d = co.model_dump()
+    c = Carewoche('test/resource_t1.json')
+    c.changeMembersOrder(d.get("ID"), d.get("offset"))
+    return c.getOrder()
+
+@app.post("/order/iterate/")
+async def change_order():
+    c = Carewoche('test/resource_t1.json')
+    c.iterateOrder()
+    return c.getOrder()
